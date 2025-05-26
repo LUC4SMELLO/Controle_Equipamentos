8bddbe6066226a2c187ca961a589bc38b830d14d
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-05-25 21:22:49 -0300

    adicionando botoes

--- frontend/interface.py
+++ frontend/interface.py
@@ -13,12 +13,61 @@ janela.geometry("1280x650")
 botao_cadastrar_equipamento = tk.Button(
     janela,
     text="Cadastrar Equipamento",
-    font=("Arial", 10, "bold"),
+    font=("Arial", 12, "bold"),
     compound="top",
     height=5,
-    width=50,
+    width=19,
 )
 botao_cadastrar_equipamento.place(x=10, y=10)
 
+botao_cadastrar_responsavel = tk.Button(
+    janela,
+    text="Cadastrar Responsável",
+    font=("Arial", 12, "bold"),
+    compound="top",
+    height=5,
+    width=19,
+)
+botao_cadastrar_responsavel.place(x=220, y=10)
+
+botao_emprestar_equipamento = tk.Button(
+    janela,
+    text="Emprestar Equipamento",
+    font=("Arial", 12, "bold"),
+    compound="top",
+    height=5,
+    width=19,
+)
+botao_emprestar_equipamento.place(x=430, y=10)
+
+botao_devolver_equipamento = tk.Button(
+    janela,
+    text="Devolver Equipamento",
+    font=("Arial", 12, "bold"),
+    compound="top",
+    height=5,
+    width=19,
+)
+botao_devolver_equipamento.place(x=640, y=10)
+
+botao_dar_baixa_equipamento = tk.Button(
+    janela,
+    text="Dar Baixa Equipamento",
+    font=("Arial", 12, "bold"),
+    compound="top",
+    height=5,
+    width=19,
+)
+botao_dar_baixa_equipamento.place(x=850, y=10)
+
+botao_dar_baixa_equipamento = tk.Button(
+    janela,
+    text="Listagem",
+    font=("Arial", 12, "bold"),
+    compound="top",
+    height=5,
+    width=19,
+)
+botao_dar_baixa_equipamento.place(x=1060, y=10)
 
 janela.mainloop()
