ec5e1c8802a7b61ed9e2c50eb0ba8614cb531365
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-05-29 12:14:22 -0300

    adicionando backend

--- frontend/frames.py
+++ frontend/frames.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import tkinter as tk
 
+from database.equipamentos import inserir_equipamento
+
 janela = tk.Tk()
 janela.title("Controle de Equipamentos")
 janela.geometry("1280x650")
--- frontend/interface.py
+++ frontend/interface.py
@@ -1,7 +1,15 @@
 from tkinter import *
+import sys
+import os
 from PIL import Image, ImageTk
 import tkinter as tk
 from tkinter import ttk
+
+import sys
+import os
+
+# Adiciona a pasta raiz do projeto ao path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 from frames import (
