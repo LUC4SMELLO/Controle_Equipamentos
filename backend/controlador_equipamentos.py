6eb31fb59a00667e0b62a09f7f1d8cc4945bafd7
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-05-29 13:10:54 -0300

    emprestrar_equipamento quase pronta

--- backend/controlador_equipamentos.py
+++ backend/controlador_equipamentos.py
@@ -3,3 +3,7 @@ from database.equipamentos import *
 
 def cadastrar_equipamento_back(gesp, codigo_modelo, modelo):
     inserir_equipamento(gesp, codigo_modelo, modelo)
+
+
+def emprestrar_equipamento_back(codigo_cliente, gesp, contrato):
+    pass
--- frontend/frames.py
+++ frontend/frames.py
@@ -97,6 +97,12 @@ botao_cadastrar_responsavel_cadastrar = tk.Button(frame_cadastro_responsavel, te
 botao_cadastrar_responsavel_cadastrar.place(x=10, y=250)
 
 
+def emprestrar_equipamento_gui():
+    codigo_cliente = entry_codigo_cliente_emprestrar.get()
+    gesp = entry_gesp_emprestar.get()
+    contrato = entry_contrato_emprestrar.get()
+
+    emprestrar_equipamento_back(codigo_cliente, gesp, contrato)
 
 # FRAME EMPRESTAR EQUIPAMENTO
 frame_emprestar_equipamento = tk.Frame(janela, width=400, height=350)
@@ -104,16 +110,26 @@ frame_emprestar_equipamento = tk.Frame(janela, width=400, height=350)
 label_emprestar_equipamento = Label(frame_emprestar_equipamento, text="EMPRESTAR EQUIPAMENTO", font=("Arial", 20, "bold"))
 label_emprestar_equipamento.place(x=10, y=10)
 
-label_codigo_cliente = tk.Label(frame_emprestar_equipamento, text="CÓDIGO CLIENTE:", font=("Arial", 15, "bold")).place(x=10, y=70)
-entry_codigo_cliente = tk.Entry(frame_emprestar_equipamento, font=("Arial", 15), width=10).place(x=195, y=70)
+label_codigo_cliente_emprestrar = tk.Label(frame_emprestar_equipamento, text="CÓDIGO CLIENTE:", font=("Arial", 15, "bold"))
+label_codigo_cliente_emprestrar.place(x=10, y=70)
 
-label_gesp_a_emprestar = tk.Label(frame_emprestar_equipamento, text="GESP:", font=("Arial", 15, "bold")).place(x=10, y=110)
-entry_gesp_a_emprestar = tk.Entry(frame_emprestar_equipamento, font=("Arial", 15), width=12).place(x=80, y=110)
+entry_codigo_cliente_emprestrar = tk.Entry(frame_emprestar_equipamento, font=("Arial", 15), width=10)
+entry_codigo_cliente_emprestrar.place(x=195, y=70)
 
-label_contrato = tk.Label(frame_emprestar_equipamento, text="CONTRATO:", font=("Arial", 15, "bold")).place(x=10, y=150)
-entry_contrato = tk.Entry(frame_emprestar_equipamento, font=("Arial", 15), width=7).place(x=140, y=150)
+label_gesp_emprestar = tk.Label(frame_emprestar_equipamento, text="GESP:", font=("Arial", 15, "bold"))
+label_gesp_emprestar.place(x=10, y=110)
 
-botao_emprestrar_equipamento = tk.Button(frame_emprestar_equipamento, text="Emprestrar", font=("Arial", 15)).place(x=10, y=210)
+entry_gesp_emprestar = tk.Entry(frame_emprestar_equipamento, font=("Arial", 15), width=12)
+entry_gesp_emprestar.place(x=80, y=110)
+
+label_contrato_emprestrar = tk.Label(frame_emprestar_equipamento, text="CONTRATO:", font=("Arial", 15, "bold"))
+label_contrato_emprestrar.place(x=10, y=150)
+
+entry_contrato_emprestrar = tk.Entry(frame_emprestar_equipamento, font=("Arial", 15), width=7)
+entry_contrato_emprestrar.place(x=140, y=150)
+
+botao_emprestrar_equipamento_emprestrar = tk.Button(frame_emprestar_equipamento, text="Emprestrar", command=emprestrar_equipamento_gui, font=("Arial", 15))
+botao_emprestrar_equipamento_emprestrar.place(x=10, y=210)
 
 
 
