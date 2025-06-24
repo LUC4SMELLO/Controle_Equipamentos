1178d3bb6d3340e3e2166fd993bb87005d18428a
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-06-24 08:29:18 -0300

    correção no nome do arquivo das binds

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from frontend.interface import iniciar_interface
 from servicos.servico_banco_de_dados.inicializador import inicializador_bancos_dados
-from backend.binds.cofiguracao_binds import configurar_todas_binds
+from backend.binds.configuracao_binds import configurar_todas_binds
 
 
 if __name__ == "__main__":
